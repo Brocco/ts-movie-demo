7d36dd567ca7ec9f16bbe4b9f6bef94db8f1347a
Mike Brocchi <mbrocchi@hotmail.com>
2014-12-03 23:51:24 -0500

    Initial commit - project setup

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+  clean = require('gulp-clean'),
+  concat = require('gulp-concat'),
+  runSequence = require('run-sequence'),
+  tsc = require('gulp-tsc');
+
+var paths = {
+  indexHtml: './app/index.html',
+  ts: './app/ts/app.ts',
+  jsLib: [
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/angular/angular.js'
+  ],
+  views: './app/views/**/*.html',
+  styles: './app/styles/**/*',
+  dist: {
+    root: './dist',
+    js: './dist/js',
+    views: './dist/views',
+    styles: './dist/styles'
+  }
+};
+
+gulp.task('default', function (callback) {
+  runSequence('clean',
+    ['build-ts', 'build-js-lib', 'build-html'],
+    callback);
+});
+
+gulp.task('build-ts', function () {
+  return gulp.src(paths.ts)
+    .pipe(tsc({
+      noResolve: false,
+      out: 'app.js',
+      outDir: paths.dist.js,
+      removeComments: true,
+      target: 'ES5'
+    }))
+    .pipe(gulp.dest(paths.dist.js))
+});
+
+gulp.task('build-js-lib', function () {
+  return gulp.src(paths.jsLib)
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest(paths.dist.js));
+});
+
+gulp.task('build-html', function () {
+  gulp.src(paths.indexHtml)
+    .pipe(gulp.dest(paths.dist.root));
+
+  return gulp.src(paths.views)
+    .pipe(gulp.dest(paths.dist.views));
+});
+
+gulp.task('build-style', function(){
+  return gulp.src(paths.styles)
+    .pipe(gulp.dest(paths.dist.styles));
+})
+
+gulp.task('clean', function () {
+  return gulp.src('./dist/*', {read: false})
+    .pipe(clean());
+});
